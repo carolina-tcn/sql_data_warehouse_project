5a3a1e9484128a5be19b9bf1ac2a64f0fc8794d2
Carolina <159081669+carolina-tcn@users.noreply.github.com>
2025-07-02 12:31:17 +0200

    Update proc_load_bronze.sql
    add CREATE OR ALTER PROCEDURE

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -16,66 +16,62 @@ Usage Example:
     EXEC bronze.load_bronze;
 ===============================================================================
 */
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+    TRUNCATE TABLE bronze.crm_cust_info;
+    BULK INSERT bronze.crm_cust_info
+    FROM '/var/opt/mssql/data/cust_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
-TRUNCATE TABLE bronze.crm_cust_info;
-BULK INSERT bronze.crm_cust_info
-FROM '/var/opt/mssql/data/cust_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
+    TRUNCATE TABLE bronze.crm_prd_info;
+    BULK INSERT bronze.crm_prd_info
+    FROM '/var/opt/mssql/data/prd_info.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
-TRUNCATE TABLE bronze.crm_prd_info;
-BULK INSERT bronze.crm_prd_info
-FROM '/var/opt/mssql/data/prd_info.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
+    TRUNCATE TABLE bronze.crm_sales_details;
+    BULK INSERT bronze.crm_sales_details
+    FROM '/var/opt/mssql/data/sales_details.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
-TRUNCATE TABLE bronze.crm_sales_details;
-BULK INSERT bronze.crm_sales_details
-FROM '/var/opt/mssql/data/sales_details.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
+    TRUNCATE TABLE bronze.erp_cust_az12;
+    BULK INSERT bronze.erp_cust_az12
+    FROM '/var/opt/mssql/data/cust_az12.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
-TRUNCATE TABLE bronze.erp_cust_az12;
-BULK INSERT bronze.erp_cust_az12
-FROM '/var/opt/mssql/data/cust_az12.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
+    TRUNCATE TABLE bronze.erp_loc_a101;
+    BULK INSERT bronze.erp_loc_a101
+    FROM '/var/opt/mssql/data/loc_a101.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
 
-TRUNCATE TABLE bronze.erp_loc_a101;
-BULK INSERT bronze.erp_loc_a101
-FROM '/var/opt/mssql/data/loc_a101.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
-
-TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM '/var/opt/mssql/data/px_cat_g1v2.csv'
-WITH (
-    FIRSTROW = 2,
-    FIELDTERMINATOR = ',',
-    TABLOCK
-);
-GO
+    TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+    BULK INSERT bronze.erp_px_cat_g1v2
+    FROM '/var/opt/mssql/data/px_cat_g1v2.csv'
+    WITH (
+        FIRSTROW = 2,
+        FIELDTERMINATOR = ',',
+        TABLOCK
+    );
+END
 
 SELECT count(*) from bronze.crm_cust_info;
 SELECT count(*) from bronze.crm_prd_info;
